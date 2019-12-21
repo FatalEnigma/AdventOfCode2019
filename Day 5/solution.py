0d55bf895c74a326d750f27e4b0f36f40ec443ff
Adam <fatal.toxicity@gmail.com>
2019-12-21 19:43:00 +0000

    Finish Day 5

--- Day 5/solution.py
+++ Day 5/solution.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from copy import copy
 
 OPCODE = namedtuple('OPCODE', 'opcode instruction_length')
 HALT = OPCODE(99, 4)
@@ -25,7 +26,7 @@ def resolve_parameter(inputs, parameter_value, parameter_mode):
         print("Unknown parameter mode: " + parameter_mode)
 
 
-def run_program(inputs):
+def run_program(inputs, manual_input=None):
     instruction_pointer = 0
     exit_program = False
 
@@ -44,7 +45,7 @@ def run_program(inputs):
                                                       resolve_parameter(inputs, instruction_pointer + 2, mode[-2])
             instruction_pointer += advance_instruction_pointer(MULTIPLY)
         elif instruction == INPUT.opcode:
-            inputs[inputs[instruction_pointer + 1]] = int(input("What is your input?: "))
+            inputs[inputs[instruction_pointer + 1]] = manual_input if manual_input else (input("What is your input?: "))
             instruction_pointer += advance_instruction_pointer(INPUT)
         elif instruction == OUTPUT.opcode:
             print(resolve_parameter(inputs, instruction_pointer + 1, mode[-1]))
@@ -60,13 +61,15 @@ def run_program(inputs):
             else:
                 instruction_pointer += advance_instruction_pointer(JUMP_IF_FALSE)
         elif instruction == LESS_THAN.opcode:
-            if resolve_parameter(inputs, instruction + 1, mode[-1]) < resolve_parameter(inputs, instruction + 2, mode[-2]):
+            if resolve_parameter(inputs, instruction_pointer + 1, mode[-1]) < \
+                    resolve_parameter(inputs, instruction_pointer + 2, mode[-2]):
                 inputs[inputs[instruction_pointer + 3]] = 1
             else:
                 inputs[inputs[instruction_pointer + 3]] = 0
             instruction_pointer += advance_instruction_pointer(LESS_THAN)
         elif instruction == EQUALS.opcode:
-            if resolve_parameter(inputs, instruction + 1, mode[-1]) == resolve_parameter(inputs, instruction + 2, mode[-2]):
+            if resolve_parameter(inputs, instruction_pointer + 1, mode[-1]) == \
+                    resolve_parameter(inputs, instruction_pointer + 2, mode[-2]):
                 inputs[inputs[instruction_pointer + 3]] = 1
             else:
                 inputs[inputs[instruction_pointer + 3]] = 0
@@ -79,10 +82,9 @@ def run_program(inputs):
     return inputs
 
 
-def part1(instructions):
-    run_program(instructions)
-
-
 if __name__ == '__main__':
     program_input = [int(x) for x in open("input.txt").read().split(',')]
-    part1(program_input)
+    print("Part 1:")
+    run_program(copy(program_input), manual_input=1)
+    print("Part 2:")
+    run_program(copy(program_input), manual_input=5)
